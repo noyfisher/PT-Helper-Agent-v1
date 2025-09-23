711f1203f1ee368d80b7cf71be4550c71d0a7308
No <noyfisher@Noys-MacBook-Pro.local>
2025-09-22 19:02:09 -0700

    Add Xcode project

---
+++ ios/PT-Helper/PT-Helper/PT_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PT_HelperApp.swift
+//  PT-Helper
+//
+//  Created by Noy Fisher on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PT_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
